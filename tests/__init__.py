38c20c0c520df5744ce60f86fa28183cacf560a6
davidsantana06 <davidsantana06@hotmail.com>
2023-11-16 14:28:03 -0300

    feat: add queue tests

--- tests/__init__.py
+++
@@ -1,6 +0,0 @@
-from os import path
-import sys
-
-
-ROOT_FOLDER = path.abspath(path.join(path.dirname(__file__), '..'))
-sys.path.append(ROOT_FOLDER)
---
+++ tests/test_queue.py
@@ -0,0 +1,51 @@
+from pytest import fixture
+from src.queue import BoundedQueue, DynamicQueue
+
+
+COMPONENTS = (
+    'Processor', 'Memory', 'Storage', 'OperatingSystem', 'NetworkInterface', 'Database',
+    'WebBrowser', 'Firewall', 'Compiler', 'Router', 'WebServer', 'GraphicsCard', 'BluetoothModule', 
+    'PeripheralDevices', 'API', 'LoadBalancer', 'DNS', 'VersionControlSystem', 'EncryptionModule', 
+    'AuthenticationService', 'BackupSystem', 'VirtualizationPlatform'
+)
+
+
+@fixture
+def bounded_queue() -> BoundedQueue:
+    queue = BoundedQueue(len(COMPONENTS))
+
+    for component in COMPONENTS:
+        queue.enqueue(component)
+
+    return queue
+
+
+@fixture
+def dynamic_queue() -> DynamicQueue:
+    queue = DynamicQueue()
+
+    for component in COMPONENTS:
+        queue.enqueue(component)
+
+    return queue
+
+
+def test_bounded_queue(bounded_queue: BoundedQueue) -> None:
+    assert bounded_queue.is_empty() is False
+    assert bounded_queue.is_full() is True
+
+    for i in range(bounded_queue.size):
+        assert bounded_queue.peek() == COMPONENTS[i]
+        assert bounded_queue.dequeue() == COMPONENTS[i]
+
+    assert bounded_queue.is_empty() is True
+
+
+def test_dynamic_queue(dynamic_queue: DynamicQueue) -> None:
+    assert dynamic_queue.is_empty() is False
+
+    for i in range(dynamic_queue.size):
+        assert dynamic_queue.peek() == COMPONENTS[i]
+        assert dynamic_queue.dequeue() == COMPONENTS[i]
+
+    assert dynamic_queue.is_empty() is True
