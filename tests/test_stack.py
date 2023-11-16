4a019e4ed0d4ff4f4d441a9a14859be5d81a90fa
davidsantana06 <davidsantana06@hotmail.com>
2023-11-16 13:02:27 -0300

    feat: add stack tests

---
+++ tests/test_stack.py
@@ -0,0 +1,51 @@
+from pytest import fixture
+from typing import List
+
+from . import *
+
+from src.stack import BoundedStack, DynamicStack
+
+
+PROCESSES = (
+    'Kernel', 'Scheduler', 'MemoryManager', 'Filesystem', 'NetworkManager', 'ProcessDispatcher',
+    'IOManager', 'SecurityDaemon', 'VirtualMachine', 'DatabaseServer', 'WebServer'
+)
+
+
+@fixture
+def bounded_stack() -> BoundedStack:
+    stack = BoundedStack(len(PROCESSES))
+
+    for process in PROCESSES:
+        stack.push(process)
+
+    return stack
+
+
+@fixture
+def dynamic_stack() -> DynamicStack:
+    stack = DynamicStack()
+
+    for process in PROCESSES:
+        stack.push(process)
+
+    return stack
+
+
+def test_bounded_stack(bounded_stack: BoundedStack) -> None:
+    assert bounded_stack.is_empty() is False
+    assert bounded_stack.is_full() is True
+
+    for i in range(bounded_stack.size):
+        assert bounded_stack.pop() == PROCESSES[-(i + 1)]
+
+    assert bounded_stack.is_empty() is True
+
+
+def test_dynamic_stack(dynamic_stack: DynamicStack) -> None:
+    assert dynamic_stack.is_empty() is False
+
+    for i in range(dynamic_stack.size):
+        assert dynamic_stack.pop() == PROCESSES[-(i + 1)]
+
+    assert dynamic_stack.is_empty() is True
